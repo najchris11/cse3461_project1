cdc81867959e07a82f1a4e8257ef246ba466ad08
coulibaly.13 <coulibaly.13@coe-dnc268474s.coeit.osu.edu>
2025-02-10 23:09:46 -0500

    looks better

--- client.c
+++ client.c
@@ -12,42 +12,53 @@
 #include <cjson/cJSON.h>
 
 #define BUFFER_LEN 1024
+ 
+void cleanup(int socketDesc, cJSON *jsonMessage, char *json_str) {
+    if (socketDesc > 0) close(socketDesc);
+    if (jsonMessage) cJSON_Delete(jsonMessage);
+    if (json_str) cJSON_free(json_str);
+}
 
 int main(int argc, char *argv[]) {
-    int portNum;
-    int socketDesc;
+    int portNum, socketDesc;
     struct sockaddr_in sin_addr;
-    char message[BUFFER_LEN];
-    char buffer[BUFFER_LEN];
+    char message[BUFFER_LEN], buffer[BUFFER_LEN];
 
-  if (argc < 3) {
-    printf("The format to run this program is client <portNumber> <ipaddress> \n");
-    exit(1);
-  }
+    if (argc < 3) {
+        printf("Usage is: client <portNumber> <ipaddress> \n");
+        return 1;
+    }
 
     portNum = atoi(argv[1]);
     char *ipAddress = argv[2];
-    if((socketDesc = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+
+    if ((socketDesc = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("error opening stream socket");
-        exit(1);
+        return 1;
     }
 
     sin_addr.sin_family = AF_INET;
     sin_addr.sin_port = htons(portNum);
 
-    if (inet_pton(AF_INET, ipAddress, &(sin_addr.sin_addr)) < 0) {
-        printf("failed to convert ip address\n");
-        close(socketDesc);
-        exit(1);
+    if (inet_pton(AF_INET, ipAddress, &(sin_addr.sin_addr)) <= 0) {
+        printf("failed to convert IP address\n");
+        cleanup(socketDesc, NULL, NULL);
+        return 1;
     }
 
-    if(connect(socketDesc, (struct sockaddr *)&sin_addr, sizeof(struct sockaddr_in)) < 0) {
-        close(socketDesc);
+    if (connect(socketDesc, (struct sockaddr *)&sin_addr, sizeof(struct sockaddr_in)) < 0) {
         perror("error connecting stream socket");
-        exit(1);
+        cleanup(socketDesc, NULL, NULL);
+        return 1;
     }
+
     printf("Please type in your desired message (less than 1024 chars): ");
-    scanf("%[^\n]s", message);
+    if (fgets(message, BUFFER_LEN, stdin) == NULL) {
+        printf("error reading input\n");
+        cleanup(socketDesc, NULL, NULL);
+        return 1;
+    }
+    message[strcspn(message, "\n")] = '\0';
 
     cJSON *jsonMessage = cJSON_CreateObject();
     cJSON_AddStringToObject(jsonMessage, "message", message);
@@ -57,65 +68,56 @@ int main(int argc, char *argv[]) {
 
     if (write(socketDesc, &convertedLength, sizeof(int)) < sizeof(int)) {
         printf("error sending message size\n");
-        cJSON_free(json_str);
-        cJSON_Delete(jsonMessage);
-        close(socketDesc);
-        exit(1);
+        cleanup(socketDesc, jsonMessage, json_str);
+        return 1;
     }
+
     if (write(socketDesc, json_str, messageSize) < messageSize) {
         printf("error sending message\n");
-        cJSON_free(json_str);
-        cJSON_Delete(jsonMessage);
-        close(socketDesc);
-        exit(1);
+        cleanup(socketDesc, jsonMessage, json_str);
+        return 1;
     }
+    
     cJSON_free(json_str);
+    json_str = NULL;
 
-
-// listen
     int receivedLength;
     if (read(socketDesc, &receivedLength, sizeof(int)) < sizeof(int)) {
-        printf("error receiving message size\n");
-        cJSON_Delete(jsonMessage);
-        close(socketDesc);
-        exit(1);
+        printf("error receiving response size\n");
+        cleanup(socketDesc, jsonMessage, NULL);
+        return 1;
     }
+
     receivedLength = ntohl(receivedLength);
-
     if (receivedLength >= BUFFER_LEN) {
-        printf("error with buffer overflow for recd message\n");
-        cJSON_Delete(jsonMessage);
-        close(socketDesc);
-        exit(1);
-    }
-    if(read(socketDesc, buffer, receivedLength) < receivedLength) {
-        printf("error receiving message\n");
-        cJSON_Delete(jsonMessage);
-        close(socketDesc);
-        exit(1);
-    }
-    jsonMessage = cJSON_Parse(buffer);
-    if (jsonMessage == NULL) {
-        const char *error_ptr = cJSON_GetErrorPtr();
-        if (error_ptr != NULL) {
-            printf("Error: %s\n", error_ptr);
-        }
-        cJSON_Delete(jsonMessage);
-        close(socketDesc);
-        exit(1);
+        printf("error: response size too large\n");
+        cleanup(socketDesc, jsonMessage, NULL);
+        return 1;
     }
 
-    cJSON *response = cJSON_GetObjectItemCaseSensitive(jsonMessage, "response");
-    if(cJSON_IsString(response) && (response->valuestring != NULL)) {
+    if (read(socketDesc, buffer, receivedLength) < receivedLength) {
+        printf("error receiving response\n");
+        cleanup(socketDesc, jsonMessage, NULL);
+        return 1;
+    }
+
+    buffer[receivedLength] = '\0';
+
+    cJSON *jsonResponse = cJSON_Parse(buffer);
+    if (jsonResponse == NULL) {
+        printf("error parsing JSON response\n");
+        cleanup(socketDesc, jsonMessage, NULL);
+        return 1;
+    }
+
+    cJSON *response = cJSON_GetObjectItemCaseSensitive(jsonResponse, "response");
+    if (cJSON_IsString(response) && response->valuestring != NULL) {
         printf("Response: %s\n", response->valuestring);
     } else {
         printf("error deserializing json");
-        close(socketDesc);
-    cJSON_Delete(jsonMessage);
-    exit(1);
     }
 
-    close(socketDesc);
-    cJSON_Delete(jsonMessage);
+    cJSON_Delete(jsonResponse);
+    cleanup(socketDesc, jsonMessage, NULL);
     return 0;
 }
